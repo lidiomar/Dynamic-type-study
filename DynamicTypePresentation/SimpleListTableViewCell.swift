52a2d8e96265868bed3ab984dd9f3ceb9a11bdea
Lidiomar Machado <lidiomarmachado@Lidiomar-Machado-MacBook-Pro-366A.local>
2022-03-25 16:26:20 -0300

    First commit

---
+++ DynamicTypePresentation/SimpleListTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  SimpleListTableViewCell.swift
+//  DynamicTypePresentation
+//
+//  Created by Lidiomar Machado on 25/03/22.
+//
+
+import UIKit
+
+class SimpleListTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var simpleListLabel: UILabel!
+    @IBOutlet weak var simpleListImage: UIImageView!
+    
+}
---
+++ DynamicTypePresentation/SimpleListViewController.swift
@@ -0,0 +1,60 @@
+//
+//  SimpleListViewController.swift
+//  DynamicTypePresentation
+//
+//  Created by Lidiomar Machado on 25/03/22.
+//
+
+import Foundation
+import UIKit
+
+struct Cuisine {
+    var image: String
+    var description: String
+}
+
+class SimpleListViewController: UIViewController {
+    
+    private let cellIdentifier = "SimpleListTableViewCell"
+    private let simpleListDataSource = [
+        Cuisine(image: "chinese", description: "Lorem ipsum dolor sit amet."),
+        Cuisine(image: "caribbean", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"),
+        Cuisine(image: "greek", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."),
+        Cuisine(image: "indian", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"),
+        Cuisine(image: "italian", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."),
+        Cuisine(image: "korean", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"),
+        Cuisine(image: "mexican", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."),
+        Cuisine(image: "moroccan", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"),
+        Cuisine(image: "peruvian", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."),
+        Cuisine(image: "thai", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua")
+    ]
+    
+    @IBOutlet private weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupTableView()
+    }
+    
+    private func setupTableView() {
+        let nib = UINib(nibName: cellIdentifier, bundle: .main)
+        tableView.register(nib, forCellReuseIdentifier: cellIdentifier)
+        tableView.dataSource = self
+    }
+}
+
+extension SimpleListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return simpleListDataSource.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier) as? SimpleListTableViewCell else {
+            return UITableViewCell()
+        }
+        let item = simpleListDataSource[indexPath.row]
+        cell.simpleListImage.image = UIImage(named: item.image)
+        cell.simpleListLabel.text = item.description
+        return cell
+    }
+}
